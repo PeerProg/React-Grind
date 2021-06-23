a773470ab4d5df6b1b575f4d30f9d578790ba9c4
oreoluwade <oreoluwade@gmail.com>
2021-06-23 11:52:58 +0100

    Display image on preview section

--- src/components/ImagePicker.js
+++ src/components/ImagePicker.js
@@ -1,29 +1,69 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState, useRef } from 'react';
+import styled, { css } from 'styled-components';
+import Spacer from './spacer';
 
 const ImagePicker = () => {
-  const [image, setImage] = useState('')
+  const [imageSrc, setImageSrc] = useState('');
+  const [filename, setFilename] = useState(null);
 
-  console.log('imageimage: ', image);
+  const fileInputRef = useRef();
+
+  const handleFileSelection = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      setImageSrc(reader.result);
+      setFilename(file.name);
+    };
+  };
+
+  const handleButtonClick = e => {
+    if (fileInputRef?.current) {
+      fileInputRef.current.click();
+    }
+  };
 
   return (
     <StyledImagePicker>
       <StyledImageSelector>
-        <label for="avatar">Choose an image to upload:</label>
+        <StyledButton onClick={handleButtonClick}>Upload a file</StyledButton>
 
-        <input type="file"
-          id="picture" name="picture"
+        <input
+          type="file"
+          ref={fileInputRef}
+          id="picture"
+          name="picture"
           accept="image/png, image/jpeg"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
+          onChange={handleFileSelection}
+          style={{ display: 'none' }}
         />
 
-        {image && <button onClick={() => setImage('')}>Remove Image</button>}
+        {imageSrc && (
+          <>
+            <Spacer height={15} />
+            <StyledButton
+              onClick={() => {
+                setImageSrc('');
+                setFilename(null);
+              }}
+              plain
+            >
+              Remove Image
+            </StyledButton>
+          </>
+        )}
       </StyledImageSelector>
-      <StyledImageScreen src={image}>Image to display</StyledImageScreen>
+      <Spacer width={40} />
+      <div>
+        <StyledImageScreen src={imageSrc} />
+        {filename && <p>{filename}</p>}
+      </div>
     </StyledImagePicker>
   );
-}
+};
 
 export default ImagePicker;
 
@@ -37,10 +77,32 @@ const StyledImageSelector = styled.div`
   flex-direction: column;
 `;
 
+const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 35px;
+  width: 140px;
+  background-color: blue;
+  border-radius: 6px;
+  color: #ffffff;
+  font-size: 14px;
+  border: none;
+  ${({ plain }) =>
+    plain &&
+    css`
+      background-color: red;
+      color: black;
+    `};
+`;
+
 const StyledImageScreen = styled.div`
   display: flex;
   height: 400px;
   width: 400px;
   ${({ src }) => src && `background-image: url(${src})`};
-  /* ${({ src }) => src && `background-color: green`} */
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: contain;
+  background-clip: border-box;
 `;
